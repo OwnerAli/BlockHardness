42ac3b6d78029dcad7f8c3f95f8033c2ad2d9d09
OwnerAli <alturfiali@gmail.com>
2023-07-04 22:47:19 -0700

    attempt fix CustomHardnessBlockBreakEvent

--- src/main/java/me/ogali/blockhardness/events/CustomHardnessBlockBreakEvent.java
+++ src/main/java/me/ogali/blockhardness/events/CustomHardnessBlockBreakEvent.java
@@ -4,33 +4,23 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.block.BlockBreakEvent;
 
-public class CustomHardnessBlockBreakEvent extends Event {
+public class CustomHardnessBlockBreakEvent extends BlockBreakEvent {
 
-    private static final HandlerList HANDLERS = new HandlerList();
-    private final Block block;
-    private final Player player;
+    private static final HandlerList handlersList = new HandlerList();
+
+    public CustomHardnessBlockBreakEvent(Block theBlock, Player player) {
+        super(theBlock, player);
+    }
 
     public static HandlerList getHandlerList() {
         return getHandlerList();
     }
 
-    public CustomHardnessBlockBreakEvent(Block theBlock, Player player) {
-        this.block = theBlock;
-        this.player = player;
-    }
-
     @Override
     public HandlerList getHandlers() {
-        return HANDLERS;
-    }
-
-    public Block getBlock() {
-        return block;
-    }
-
-    public Player getPlayer() {
-        return player;
+        return handlersList;
     }
 
 }
--- src/main/java/me/ogali/blockhardness/listeners/PlayerJoinListener.java
+++ src/main/java/me/ogali/blockhardness/listeners/PlayerJoinListener.java
@@ -1,16 +1,7 @@
 package me.ogali.blockhardness.listeners;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedEnumEntityUseAction;
 import me.ogali.blockhardness.BlockHardnessPlugin;
 import me.ogali.blockhardness.player.domain.BreakPlayer;
-import me.ogali.blockhardness.runnables.BlockBreakRunnable;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
