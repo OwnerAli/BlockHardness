6ae128a92d0e6f05cc49ccf3e9f19fcab6098668
OwnerAli <alturfiali@gmail.com>
2023-07-04 22:57:48 -0700

    attempt fix CustomHardnessBlockBreakEvent

--- src/main/java/me/ogali/blockhardness/events/CustomHardnessBlockBreakEvent.java
+++ src/main/java/me/ogali/blockhardness/events/CustomHardnessBlockBreakEvent.java
@@ -2,25 +2,23 @@ package me.ogali.blockhardness.events;
 
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class CustomHardnessBlockBreakEvent extends BlockBreakEvent {
 
-    private static final HandlerList handlersList = new HandlerList();
+    private static final HandlerList handlers = new HandlerList();
 
     public CustomHardnessBlockBreakEvent(Block theBlock, Player player) {
         super(theBlock, player);
     }
 
-    public static HandlerList getHandlerList() {
-        return getHandlerList();
+    public HandlerList getHandlers() {
+        return handlers;
     }
 
-    @Override
-    public HandlerList getHandlers() {
-        return handlersList;
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 
 }
